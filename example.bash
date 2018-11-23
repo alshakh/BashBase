e41877e417417fb7658d5b5af7139ced673c7cde
Ahmed Alshakh <ahmed.s.alshakh@gmail.com>
2018-11-24 01:33:34 +0300

    standarize funciton names

--- example.bash
+++ example.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # load the lib
-source "$(dirname "$0")/base.bash"
+source "$(dirname "$0")/bashbase"
 
 
 db_file=/tmp/example.db
@@ -10,7 +10,7 @@ touch "$db_file" # empty db
 
 
 
-bb_db_open "$db_file"
+bbdb_open "$db_file"
 
 
 
@@ -19,7 +19,7 @@ bb_db_open "$db_file"
 
 #======= constructing entries ========
 echo "# constructing entry from  'firstname=john M.' 'lastname=smith'"
-entry_0="$( bb_db_construct_entry "firstname=john M." "lastname=smith"  )"
+entry_0="$( bbdb_construct_entry "firstname=john M." "lastname=smith"  )"
 echo "$entry_0"
 #--> firstname:!:john M.:%:lastname:!:smith
 
@@ -27,56 +27,56 @@ echo "$entry_0"
 #======= extracting values from entreis ========
 echo
 echo "# extracting 'firstname' from previous entry"
-bb_db_extract_value_from_entry "$entry_0" 'firstname'
+bbdb_extract_value_from_entry "$entry_0" 'firstname'
 #--> john M.
 echo "# extracting 'lastname' from previous entry"
-bb_db_extract_value_from_entry "$entry_0" 'lastname'
+bbdb_extract_value_from_entry "$entry_0" 'lastname'
 #--> smith
 
 
 #======= querying db ========
 echo 
 echo "# quering db for 'lastname=smith' without db entry"
-bb_db_query "lastname=smith"
+bbdb_query "lastname=smith"
 # NOTHING
 
 
 echo "# quering db for 'lastname=smith' after inserting entry to db"
-bb_db_commit_entry "$entry_0"
-bb_db_query "lastname=smith"
+bbdb_commit_entry "$entry_0"
+bbdb_query "lastname=smith"
 #--> firstname:!:john M.:%:lastname:!:smith
 
 echo "# add a different entry to db, same lastname"
-bb_db_commit_entry "$(bb_db_construct_entry "firstname=jena K." "lastname=smith")"
+bbdb_commit_entry "$(bbdb_construct_entry "firstname=jena K." "lastname=smith")"
 
 echo "# quering db for 'lastname=smith'"
-bb_db_query "lastname=smith"
+bbdb_query "lastname=smith"
 #--> firstname:!:john M.:%:lastname:!:smith
 #--> firstname:!:jena K.:%:lastname:!:smith
 
 echo "# quering is actually regex (gawk rules) with 'lastname=s.*'"
-bb_db_query "lastname=s.*"
+bbdb_query "lastname=s.*"
 #--> firstname:!:john M.:%:lastname:!:smith
 #--> firstname:!:jena K.:%:lastname:!:smith
 
 echo "# quering is actually regex (gawk rules) with 'lastname=s.*' and 'firstname=jena.*'"
-bb_db_query "lastname=s.*" "firstname=jena.*"
+bbdb_query "lastname=s.*" "firstname=jena.*"
 #--> firstname:!:jena K.:%:lastname:!:smith
 
 
 #======= deleting db ========
 echo 
 echo "# dump whole db"
-bb_db_dump 
+bbdb_dump 
 #--> firstname:!:john M.:%:lastname:!:smith
 #--> firstname:!:jena K.:%:lastname:!:smith
 
 
 echo "# deleting jena's entry"
-bb_db_delete_entry "$(bb_db_query "firstname=jena.*" | head -n 1)"
+bbdb_delete_entry "$(bbdb_query "firstname=jena.*" | head -n 1)"
 
 echo "# dump whole db"
-bb_db_dump 
+bbdb_dump 
 #--> firstname:!:john M.:%:lastname:!:smith
 
 
@@ -87,22 +87,22 @@ echo "$entry_0"
 #--> firstname:!:john M.:%:lastname:!:smith
 
 echo "# add a field without commiting"
-bb_db_modify_entry "$entry_0" "age" "42"
+bbdb_modify_entry "$entry_0" "age" "42"
 #--> firstname:!:john M.:%:lastname:!:smith:%:age:!:42
 
 echo "# entry did not change in db"
-bb_db_query "lastname=smith" "firstname=john.*"
+bbdb_query "lastname=smith" "firstname=john.*"
 #--> firstname:!:john M.:%:lastname:!:smith
 
 echo "# change lastname without commiting"
-bb_db_modify_entry "$entry_0" "lastname" "doe"
+bbdb_modify_entry "$entry_0" "lastname" "doe"
 #--> firstname:!:john M.:%:lastname:!:doe
 
 
 echo "# change lastname and commit "
-bb_db_modify_entry_and_commit "$entry_0" "lastname" "doe"
+bbdb_modify_entry_and_commit "$entry_0" "lastname" "doe"
 
 echo "# dump whole db"
-bb_db_dump
+bbdb_dump
 #--> firstname:!:john M.:%:lastname:!:doe
 
